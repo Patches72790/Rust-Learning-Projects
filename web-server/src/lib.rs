b59042b6794c07c650334526df232c231ed06425
Patroclus <patches72790@gmail.com>
2022-01-14 23:15:59 -0600

    added worker and thread pool early implementation

--- web-server/src/lib.rs
+++ web-server/src/lib.rs
@@ -1,1 +1,44 @@
-mod routes;
+use std::thread;
+use std::{
+    net::TcpStream,
+    thread::{JoinHandle, Thread},
+};
+
+struct Worker {
+    id: usize,
+    thread: JoinHandle<()>,
+}
+
+impl Worker {
+    pub fn new(id: usize) -> Worker {
+        Worker {
+            id,
+            thread: thread::spawn(|| {}),
+        }
+    }
+}
+
+pub struct ThreadPool {
+    threads: Vec<Worker>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+        let mut threads = Vec::with_capacity(size);
+
+        for id in 0..size {
+            threads.push(Worker::new(id));
+        }
+
+        ThreadPool { threads }
+    }
+
+    pub fn execute<F, T>(&self, f: F)
+    where
+        F: FnOnce() -> T,
+        F: Send + 'static,
+        T: Send + 'static,
+    {
+    }
+}
--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -4,23 +4,28 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+use web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(move || handle_connection(&stream));
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: &TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
     let get_request = b"GET / HTTP/1.1\r\n";
     let get_favicon = b"GET /favicon.ico HTTP/1.1\r\n";
+    let get_sleep = b"GET /sleep HTTP/1.1\r\n";
 
     if buffer.starts_with(get_request) {
         let contents = fs::read_to_string("./public/index.html").unwrap();
@@ -47,6 +52,15 @@ fn handle_connection(mut stream: TcpStream) {
         );
 
         stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else if buffer.starts_with(get_sleep) {
+        thread::sleep(Duration::from_secs(10));
+
+        let response = format!("HTTP/1.1 200 OK\r\n");
+
+        stream
+            .write(response.as_bytes())
+            .expect("Error writing response");
         stream.flush().unwrap();
     } else {
         let contents = fs::read_to_string("./public/404.html").expect("Error reading HTML file!");
