3a9a2b4ae3774e83011e66967bb4dd50a4005fa8
Patrick Harvey <patrick.harvey@wisc.edu>
2023-08-28 11:11:02 -0500

    added dockerfile

--- axum-api-project/backend/src/main.rs
+++ axum-api-project/backend/src/main.rs
@@ -6,14 +6,18 @@ use sqlx::postgres::PgPoolOptions;
 async fn main() -> Result<(), sqlx::Error> {
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .acquire_timeout(Duration::from_secs(5))
-        .connect("postgres://postgres:password@localhost/test")
+        .acquire_timeout(Duration::from_secs(60))
+        .connect("postgres://user:password@postgres/postgres")
         .await?;
+
+    sqlx::migrate!("db/migrations");
 
     let row: (i64,) = sqlx::query_as("SELECT $1")
         .bind(150_i64)
         .fetch_one(&pool)
         .await?;
+
+    println!("{:?}", row);
 
     Ok(())
 }
